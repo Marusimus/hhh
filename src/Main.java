619a617ffad5022ab05d372fd8c132d3400108e9
Maria <marder1995@mail.ru>
2025-02-25 20:55:05 +0300

    last version

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         int balance = 100;
-        int amound = 1100;
+        int amound = 1000;
         int bonus;
-        boolean b = amound >= 1000;
+        boolean actionParticipant = amound > 1000;
 
-        if (b) {
+        if (actionParticipant) {
             bonus = amound / 100;
             int total = amound + bonus + balance;
             System.out.println("Количество бонусных рублей: " + bonus + " Итоговый счет: " + total);
-            } else {
+        } else {
             bonus = 0;
             int total = amound + bonus + balance;
             System.out.println("Количество бонусных рублей: " + bonus + " Итоговый счет: " + total);
